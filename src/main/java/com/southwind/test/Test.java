d91d1e0879273011e988d711b88e8dace2fe000f
GloriaKY <695198530@qq.com>
2019-12-27 13:42:32 +0800

    继承

--- src/main/java/com/southwind/test/Test.java
+++ src/main/java/com/southwind/test/Test.java
@@ -2,6 +2,7 @@ package com.southwind.test;
 
 import com.southwind.entity.Classes;
 import com.southwind.entity.Student;
+import com.southwind.entity.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,13 +11,9 @@ public class Test {
     public static void main(String[] args) {
         //加载ioc容器
         ApplicationContext applicationContext=new ClassPathXmlApplicationContext("spring.xml");
-      System.out.println(applicationContext);
-//      Student stu=(Student)applicationContext.getBean("stu");
-//        //只能保证一个对象
-//        Student stu=applicationContext.getBean(Student.class);
-        Student stu2=(Student)applicationContext.getBean("stu2");
-       System.out.println(stu2);
-        Classes classes=(Classes)applicationContext.getBean("classes1");
-System.out.println(classes);
+      User user=(User)applicationContext.getBean("user");
+      System.out.println(user);
+      User user2=(User)applicationContext.getBean("user2");
+      System.out.println(user2);
     }
 }
